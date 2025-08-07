19006adae8002543be9e05e2f9e3230e55cd5428
zannat671 <beingzannat24@gmail.com>
2025-08-07 13:16:37 +0600

    admin panel menu done

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
-//nn
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int login(void)
 {
-    printf("Welcome!");
+    char user[20], pass[20];
 
-} 
+    printf("=========== Login ===========\n");
+    printf("Username: ");
+    scanf("%19s", user);
+    printf("Password: ");
+    scanf("%19s", pass);
+
+    if (strcmp(user, "admin") == 0 && strcmp(pass, "1234") == 0)
+    {
+        printf("Login successful.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int main(){
+
+    login();
+
+}
